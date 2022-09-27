cd12dd4777a592ea3296faf9c986c0ce08f91d9e
Lucas Lacerda <lucas.lacerda@bandtec.com.br>
2022-09-27 19:14:26 -0300

    <fixes> fixed import

--- src/main/java/controly/repository/TopicoRepository.java
+++ src/main/java/controly/repository/TopicoRepository.java
@@ -1,7 +1,6 @@
 package controly.repository;
 
 import controly.model.entity.TopicoEntity;
-import controly.model.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface TopicoRepository extends JpaRepository<TopicoEntity,Long> {
