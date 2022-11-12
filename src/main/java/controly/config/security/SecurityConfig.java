2d5f7fa3dbd226a1d8fe19953a7708a74a2249db
Don616 <contatodonilo@gmail.com>
2022-11-11 21:28:39 -0300

    subindo cors

--- src/main/java/controly/config/security/SecurityConfig.java
+++ src/main/java/controly/config/security/SecurityConfig.java
@@ -18,6 +18,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                .cors()
+                .and()
                 .httpBasic()
                 .and()
                 .authorizeHttpRequests()
