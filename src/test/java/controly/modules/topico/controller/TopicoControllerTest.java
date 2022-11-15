d82229b1eeb0aabc62ea85acdaafe1ac4b83dab2
Lucas Lacerda <lucas.lacerda@bandtec.com.br>
2022-11-15 17:21:02 -0300

    fix: Criando modelo para testes unitarios

---
+++ src/test/java/controly/modules/topico/controller/TopicoControllerTest.java
@@ -0,0 +1,49 @@
+package controly.modules.topico.controller;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class TopicoControllerTest {
+
+    @Test
+    @DisplayName("Devera retornar um 201 e seguir o topico")
+    void getTopicoSucess() {
+    }
+
+    @Test
+    @DisplayName("Devera retornar um 201 e seguir o topico")
+    void getTopicoFailed() {
+    }
+
+    @Test
+    @DisplayName("Devera retornar um 201 e seguir o topico")
+    void getTopicosSucess() {
+    }
+
+    @Test
+    @DisplayName("Devera retornar um 201 e seguir o topico")
+    void getTopicosFailed() {
+    }
+
+    @Test
+    @DisplayName("Devera retornar um 201 e seguir o topico")
+    void followTopicoSuccess() {
+    }
+
+    @Test
+    @DisplayName("Devera retornar um 400 e não seguir um topico")
+    void followTopicoFailed() {
+    }
+
+    @Test
+    @DisplayName("Devera retornar um 201 e seguir o topico")
+    void unfollowTopicoSucess() {
+    }
+
+    @Test
+    @DisplayName("Devera retornar um 201 e seguir o topico")
+    void postTopicosFailed() {
+    }
+}
