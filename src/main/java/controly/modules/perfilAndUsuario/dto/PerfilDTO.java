7add264f61351841f232beb0e7788027ec1f44be
swtt-pch <igor.gsouza@bandtec.com.br>
2022-11-23 18:21:35 -0300

    feat: perfil postagem mais pontuada :sparkles:

--- src/main/java/controly/modules/perfilAndUsuario/dto/PerfilDTO.java
+++ src/main/java/controly/modules/perfilAndUsuario/dto/PerfilDTO.java
@@ -1,11 +1,14 @@
 package controly.modules.perfilAndUsuario.dto;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import controly.modules.topico.entities.TopicoEntity;
 import controly.modules.topico.entities.TopicoHasSeguidoresEntity;
 import controly.modules.perfilAndUsuario.entities.UsuarioEntity;
 import controly.modules.postagem.entities.PostagemEntity;
 import lombok.Data;
 
+import java.util.Comparator;
 import java.util.List;
 
 @Data
@@ -14,4 +17,8 @@ public class PerfilDTO {
     private UsuarioEntity usuario;
     private List<PostagemEntity> postagens;
     private List<TopicoEntity> topicos_seguidos;
+    @JsonProperty
+    public PostagemEntity getPostagemMaisPontuada(){
+        return postagens.stream().max(Comparator.comparing(PostagemEntity::getPontuacao)).orElse(null);
+    }
 }
