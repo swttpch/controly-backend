f5395f009beb070d9b2d2ac99c343312c7a82a8a
swtt-pch <i.greygomes3@gmail.com>
2022-12-14 21:55:53 -0300

    fix/refactor: login auth

--- src/main/java/controly/service/AuthService.java
+++ src/main/java/controly/service/AuthService.java
@@ -3,10 +3,10 @@ package controly.service;
 
 import controly.dto.GithubUserRequest;
 import controly.entities.UserEntity;
+import controly.exception.UsersEmailNotFould;
 import controly.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.server.ResponseStatusException;
 
@@ -14,18 +14,15 @@ import java.util.Optional;
 
 @Service
 public class AuthService {
-
     @Autowired
     UserRepository userRepository;
 
-    public ResponseEntity<UserEntity> login(String email, String senha) {
+    public UserEntity login(String email, String senha) {
         UserEntity user = userRepository.findByEmail(email)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND,"Email não achado"));
-
+                .orElseThrow(UsersEmailNotFould::new);
         if (!user.getPassword().equals(senha))
-            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"Senha incorreta");
-        return ResponseEntity.status(200).body(user);
-
+            throw new ResponseStatusException(HttpStatus.UNAUTHORIZED,"Wrong password.");
+        return user;
     }
 
     public UserEntity githubAuth(GithubUserRequest githubUser) {
