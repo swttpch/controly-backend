170fceccaf40b216485734db1ad88e309576a3a7
swtt-pch <i.greygomes3@gmail.com>
2022-12-21 01:04:00 -0300

    clear: cleaning utils

--- src/main/java/controly/util/PasswordGenerator.java
+++ src/main/java/controly/util/PasswordGenerator.java
@@ -7,22 +7,22 @@ import java.util.Random;
 public class PasswordGenerator {
 
     private String newPassword = "#";
-    private String[] upperCaseLetters = {"A","B","C","D"
+    private final String[] upperCaseLetters = {"A","B","C","D"
             ,"E","F","G","H","I","J","K","L","M"
             ,"N","O","P","Q","R","S","T","U","V"
             ,"W","X","Y","Z"};
-    private String[] lowerCaseLetters = {"a","b","c","d"
+    private final String[] lowerCaseLetters = {"a","b","c","d"
             ,"e","f","g","h","i","j","k","l","m"
             ,"n","o","p","q","r","s","t","u","v"
             ,"w","x","y","z"};
-    private String[] symbols = {"!","@","$","%","&"};
-    private Integer[] numbers = {0,1,2,3,4,5,6,7,8,9};
+    private final String[] symbols = {"!","@","$","%","&"};
+    private final Integer[] numbers = {0,1,2,3,4,5,6,7,8,9};
 
     public String generate(){
         Random random = new Random();
         for(int i = 0; i < 14; i++){
 
-            Integer option = random.nextInt(3);
+            int option = random.nextInt(3);
             Integer randomLetter = random.nextInt(25);
             Integer randomSymbol = random.nextInt(5);
             Integer randomNumber = random.nextInt(9);
@@ -50,40 +50,40 @@ public class PasswordGenerator {
 
     }
 
-    private String getUpperCaseLetter(Integer posicao){
+    private String getUpperCaseLetter(Integer position){
 
         for(int i = 0; i < upperCaseLetters.length; i++){
-            if(i==posicao){
+            if(i==position){
                 return upperCaseLetters[i];
             }
         }
         return "A";
     }
 
-    private String getLowerCaseLetter(Integer posicao){
+    private String getLowerCaseLetter(Integer position){
 
         for(int i = 0; i < lowerCaseLetters.length; i++){
-            if(i==posicao){
+            if(i==position){
                 return lowerCaseLetters[i];
             }
         }
         return "a";
     }
 
-    private String getSymbol(Integer posicao){
+    private String getSymbol(Integer position){
 
         for(int i = 0; i < symbols.length; i++){
-            if(i==posicao){
+            if(i==position){
                 return symbols[i];
             }
         }
         return "@";
     }
 
-    private Integer getNumber(Integer posicao){
+    private Integer getNumber(Integer position){
 
         for(int i = 0; i < numbers.length; i++){
-            if(i==posicao){
+            if(i==position){
                 return numbers[i];
             }
         }
