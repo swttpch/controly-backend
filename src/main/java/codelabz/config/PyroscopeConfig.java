bb6d8ee467c780c38ce50238a5385b7c693ad7f7
donilo.pontes <donilo.pontes@vericode.com.br>
2023-10-02 21:50:45 -0300

    add

---
+++ src/main/java/codelabz/config/PyroscopeConfig.java
@@ -0,0 +1,24 @@
+package codelabz.config;
+import io.pyroscope.javaagent.PyroscopeAgent;
+import io.pyroscope.javaagent.config.Config;
+import io.pyroscope.javaagent.EventType;
+import io.pyroscope.http.Format;
+
+import javax.annotation.PostConstruct;
+
+public class PyroscopeConfig {
+
+    @PostConstruct
+    public void init() {
+
+        PyroscopeAgent.start(
+                new Config.Builder()
+                        .setApplicationName("ride-sharing-app-java")
+                        .setProfilingEvent(EventType.ITIMER)
+                        .setFormat(Format.JFR)
+                        .setServerAddress("http://localhost:4040")
+                        .build()
+        );
+    }
+
+}
